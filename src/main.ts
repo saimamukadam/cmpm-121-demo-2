14d374b085d2ee649e62981f51febbf02cde9f3b
Saima <mukadam.saima@gmail.com>
2024-10-22 20:43:01 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,27 @@ import "./style.css";
 const APP_NAME = "Saima's sticker sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+// document.title = APP_NAME;
+// app.innerHTML = APP_NAME;
+//  ^ commented to remove small title text
+
+// adding app title in h1 element
+const header = document.createElement("h1");
+header.innerHTML = APP_NAME;
+app.append(header);
+
+// adding a canvas
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
+
+const ctx = canvas.getContext("2d");
+
+if (!ctx) {
+    throw new Error("no canvas context");
+}
+
+ctx.fillStyle = "pink";
+ctx.fillRect(10, 10, 150, 100);
