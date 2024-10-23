70bf642cadcfeca74cde5b51843ec29c095293ac
Saima <mukadam.saima@gmail.com>
2024-10-23 01:51:42 -0700

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -30,45 +30,82 @@ ctx.fillRect(10, 10, 150, 100);
 
 // marker drawing on canvas
 let isDrawing = false;
-let x = 0;
-let y = 0;
+// let x = 0;
+// let y = 0;
+let points: Array<Array<{ x: number, y: number }>> = [[]]; 
+let currentLine: { x: number, y: number }[] = [];
 
 canvas.addEventListener("mousedown", (e: MouseEvent) => {
-    x = e.offsetX;
-    y = e.offsetY;
+    // x = e.offsetX;
+    // y = e.offsetY;
     isDrawing = true;
+    currentLine = [{ x: e.offsetX, y: e.offsetY }];
 });
 
 canvas.addEventListener("mousemove", (e: MouseEvent) => {
     if (isDrawing) {
-        drawLine(ctx, x, y, e.offsetX, e.offsetY);
-        x = e.offsetX;
-        y = e.offsetY;
+        // drawLine(ctx, x, y, e.offsetX, e.offsetY);
+        // x = e.offsetX;
+        // y = e.offsetY;
+        currentLine.push({ x: e.offsetX, y: e.offsetY });
+        const event = new CustomEvent("drawing changed");
+        canvas.dispatchEvent(event);
     }
 });
 
 canvas.addEventListener("mouseup", () => {
-    isDrawing = false;
+    // isDrawing = false;
+    if (isDrawing) {
+        points.push(currentLine);
+        isDrawing = false;
+    }
 });
 
 canvas.addEventListener("mouseout", () => {
     isDrawing = false;
 });
 
-function drawLine(ctx: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
-    ctx.beginPath();
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 1;
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-    ctx.closePath();
+canvas.addEventListener("drawing changed", () => {
+    clearAndRedraw();
+});
+
+function clearAndRedraw() {
+    if (!ctx) return; // had to add so ctx errors go away
+
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.fillStyle = "pink";
+    ctx.fillRect(10, 10, 150, 100); 
+
+    for (const line of points) {
+        if (line.length > 0) {
+            ctx.beginPath();
+            ctx.moveTo(line[0].x, line[0].y);
+            for (const point of line) {
+                ctx.lineTo(point.x, point.y);
+            }
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 1;
+            ctx.stroke();
+            ctx.closePath();
+        }
+    }
 }
+
+// function drawLine(ctx: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+//     ctx.beginPath();
+//     ctx.strokeStyle = "black";
+//     ctx.lineWidth = 1;
+//     ctx.moveTo(x1, y1);
+//     ctx.lineTo(x2, y2);
+//     ctx.stroke();
+//     ctx.closePath();
+// }
 
 // add a clear button
 const clearButton = document.createElement("button");
 clearButton.innerHTML = "Clear Canvas";
 clearButton.addEventListener("click", () => {
+    points = [];
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 app.append(clearButton);
